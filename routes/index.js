9b948287b1d008937d02e0b787d97ba713306df1
pratikraidev <raipratikcse@gmail.com>
2025-04-15 23:22:24 +0530

    error handling

--- routes/index.js
+++ routes/index.js
@@ -13,126 +13,166 @@ router.post("/download-invoice", async (req, res, next) => {
   const createDate = getDate();
   const invoiceDate = getDate(req.body.invoiceDate);
   const invoiceData = { ...req.body, createDate, invoiceDate };
-  //const colRef = await db.collection("invoice").add(invoiceData);
-  // pdf creation
-  const fs = require("fs");
-  const PDFDocument = require("pdfkit-table");
-  const doc = new PDFDocument({ size: "A4", margin: 0 });
-  doc.registerFont("DejaVuSans", "public/DejaVuSans.ttf");
+  try {
+    const colRef = await db.collection("invoice").add(invoiceData);
+    // pdf creation
+    const fs = require("fs");
+    const PDFDocument = require("pdfkit-table");
+    const doc = new PDFDocument({ size: "A4", margin: 0 });
+    doc.registerFont("DejaVuSans", "public/DejaVuSans.ttf");
 
-  // Output file
-  const stream = fs.createWriteStream("public/output.pdf");
-  doc.pipe(stream);
+    // Output file
+    const stream = fs.createWriteStream("public/output.pdf");
+    doc.pipe(stream);
 
-  // Dark Purple Left Section
-  doc.rect(0, 0, 249, 30).fill("#2E0052");
+    // Dark Purple Left Section
+    doc.rect(0, 0, 249, 30).fill("#2E0052");
 
-  // White Slant Divider
-  doc.moveTo(250, 0).lineTo(210, 30).lineTo(250, 30).fillAndStroke("white");
+    // White Slant Divider
+    doc.moveTo(250, 0).lineTo(210, 30).lineTo(250, 30).fillAndStroke("white");
 
-  doc.moveTo(260, 0).lineTo(240, 15).lineTo(270, 15).fill("#3A5ADB");
+    doc.moveTo(260, 0).lineTo(240, 15).lineTo(270, 15).fill("#3A5ADB");
 
-  // Blue Section
-  doc.rect(260, 0, 866, 15).fill("#3A5ADB");
-  doc.font("Helvetica-Bold").text(`GST NO - ${invoiceData.gst}`, 20, 40);
+    // Blue Section
+    doc.rect(260, 0, 866, 15).fill("#3A5ADB");
+    doc.font("Helvetica-Bold").text(`GST NO - ${invoiceData.gst}`, 20, 40);
 
-  doc.moveDown();
-  doc.image("public/logo.png", 470, 30, { width: 100, height: 74 }).moveDown();
+    doc.moveDown();
+    doc
+      .image("public/logo.png", 470, 30, { width: 100, height: 74 })
+      .moveDown();
 
-  doc
-    .font("Helvetica-Bold")
-    .fillColor("#4A148C")
-    .text("SOLUTION FOR POWERFUL HEARING AND COMMUNICATION", 220, 110, {})
-    .moveDown();
+    doc
+      .font("Helvetica-Bold")
+      .fillColor("#4A148C")
+      .text("SOLUTION FOR POWERFUL HEARING AND COMMUNICATION", 220, 110, {})
+      .moveDown();
 
-  doc.font("Helvetica").fillColor("black").text("Billed To :", 20);
-  doc.font("Helvetica-Bold").text(invoiceData.customerName);
-  doc.font("Helvetica").fontSize(10).text(invoiceData.addressline1);
-  if (invoiceData.addressline2) {
-    doc.font("Helvetica").fontSize(10).text(invoiceData.addressline2);
-  }
-  doc
-    .font("Helvetica")
-    .fontSize(10)
-    .text(`${invoiceData.city}, ${invoiceData.state} - ${invoiceData.pin}`);
-  if (invoiceData.phone) {
-    doc.font("Helvetica").fontSize(10).text(`+91-${invoiceData.phone}`);
-  }
-  doc
-    .font("Helvetica")
-    .fontSize(10)
-    .text(getAddress(invoiceData.sophecoAddress), 280, 130, {
-      width: 300,
-      align: "right",
+    doc.font("Helvetica").fillColor("black").text("Billed To :", 20);
+    doc.font("Helvetica-Bold").text(invoiceData.customerName);
+    doc.font("Helvetica").fontSize(10).text(invoiceData.addressline1);
+    if (invoiceData.addressline2) {
+      doc.font("Helvetica").fontSize(10).text(invoiceData.addressline2);
+    }
+    doc
+      .font("Helvetica")
+      .fontSize(10)
+      .text(`${invoiceData.city}, ${invoiceData.state} - ${invoiceData.pin}`);
+    if (invoiceData.phone) {
+      doc.font("Helvetica").fontSize(10).text(`+91-${invoiceData.phone}`);
+    }
+    doc
+      .font("Helvetica")
+      .fontSize(10)
+      .text(getAddress(invoiceData.sophecoAddress), 280, 130, {
+        width: 300,
+        align: "right",
+      });
+
+    doc
+      .font("Helvetica-Bold")
+      .fontSize(11)
+      .text(invoiceData.invoiceNumber, 500, 200);
+    doc.font("Helvetica-Bold").fontSize(11).text(createDate, 516, 212);
+
+    doc.font("Helvetica").fontSize(12).text("", 20, 250);
+
+    const table = {
+      headers: getHeaders(),
+      datas: getRowData(invoiceData),
+    };
+
+    doc.table(table, {
+      prepareHeader: () =>
+        doc.font("Helvetica-Bold").fontSize(12).fillColor("white"),
+      prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+        doc.font("Helvetica").fontSize(12).fillColor("black");
+      },
+      width: 800,
+      padding: 15,
+      divider: {
+        header: { disabled: true },
+        horizontal: { disabled: true, width: 0.1, opacity: 1 },
+        vertical: { width: 1, opacity: 1 },
+      },
     });
+    doc
+      .font("Helvetica")
+      .fontSize(10)
+      .text("Amount chargeable (in words)", 30, 600);
+    doc
+      .font("Helvetica-Bold")
+      .fontSize(10)
+      .text(`INR ${toWords(+invoiceData.totalAmount)}`, { width: 400 });
+    doc
+      .font("Helvetica-Bold")
+      .fontSize(15)
+      .text("SOPHECO PRIVATE LIMITED.", 30, 660);
+    doc.font("Helvetica").fontSize(15).text("Company's PAN: ABMCS2566D");
+    doc
+      .font("Helvetica")
+      .fontSize(10)
+      .text(
+        `We declare that this invoice shows the actual price of the goods\ndescribed and that all particulars are true and correct\n1) 100% payment against delivery\n2) delivery within 3 working days after making payment\n`,
+        30,
+        700
+      );
+    doc.font("Helvetica-Bold").fontSize(8).text(`*Terms and conditions apply`);
+    doc.image("public/sign.png", 380, 590, { width: 200, height: 184 });
+    doc.fontSize(10).text("Contact No:- (+91-9006164420)", 410, 780);
+    // total border
+    doc
+      .moveTo(570, 560)
+      .lineTo(30, 560)
+      .strokeOpacity(0.5)
+      .strokeColor("black");
+    doc
+      .moveTo(570, 590)
+      .lineTo(30, 590)
+      .strokeOpacity(0.5)
+      .strokeColor("black");
+    // total border
 
-  doc
-    .font("Helvetica-Bold")
-    .fontSize(11)
-    .text(invoiceData.invoiceNumber, 500, 200);
-  doc.font("Helvetica-Bold").fontSize(11).text(createDate, 516, 212);
-
-  doc.font("Helvetica").fontSize(12).text("", 20, 250);
-
-  const table = {
-    headers: getHeaders(),
-    datas: getRowData(invoiceData),
-  };
-
-  doc.table(table, {
-    prepareHeader: () =>
-      doc.font("Helvetica-Bold").fontSize(12).fillColor("white"),
-    prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
-      doc.font("Helvetica").fontSize(12).fillColor("black");
-    },
-    width: 800,
-    padding: 15,
-    divider: {
-      header: { disabled: true },
-      horizontal: { disabled: true, width: 0.1, opacity: 1 },
-      vertical: { width: 1, opacity: 1 },
-    },
-  });
-  doc
-    .font("Helvetica")
-    .fontSize(10)
-    .text("Amount chargeable (in words)", 30, 600);
-  doc
-    .font("Helvetica-Bold")
-    .fontSize(10)
-    .text(`INR ${toWords(+invoiceData.totalAmount)}`, { width: 400 });
-  doc
-    .font("Helvetica-Bold")
-    .fontSize(15)
-    .text("SOPHECO PRIVATE LIMITED.", 30, 660);
-  doc.font("Helvetica").fontSize(15).text("Company's PAN: ABMCS2566D");
-  doc
-    .font("Helvetica")
-    .fontSize(10)
-    .text(
-      `We declare that this invoice shows the actual price of the goods\ndescribed and that all particulars are true and correct\n1) 100% payment against delivery\n2) delivery within 3 working days after making payment\n`,
-      30,
-      700
+    //row border
+    doc
+      .moveTo(430, 560)
+      .lineTo(430, 280)
+      .strokeOpacity(0.5)
+      .strokeColor("black");
+    doc
+      .moveTo(350, 560)
+      .lineTo(350, 280)
+      .strokeOpacity(0.5)
+      .strokeColor("black");
+    doc
+      .moveTo(300, 560)
+      .lineTo(300, 280)
+      .strokeOpacity(0.5)
+      .strokeColor("black");
+    doc
+      .moveTo(220, 560)
+      .lineTo(220, 280)
+      .strokeOpacity(0.5)
+      .strokeColor("black");
+    doc.stroke();
+    //row border
+    console.log(
+      `${invoiceData.customerName.replaceAll(
+        " ",
+        "_"
+      )}_${invoiceData.invoiceNumber.replaceAll("/", "_")}.pdf`
     );
-  doc.font("Helvetica-Bold").fontSize(8).text(`*Terms and conditions apply`);
-  doc.image("public/sign.png", 380, 590, { width: 200, height: 184 });
-  doc.fontSize(10).text("Contact No:- (+91-9006164420)", 410, 780);
-  // total border
-  doc.moveTo(570, 560).lineTo(30, 560).strokeOpacity(0.5).strokeColor("black");
-  doc.moveTo(570, 590).lineTo(30, 590).strokeOpacity(0.5).strokeColor("black");
-  // total border
-
-  //row border
-  doc.moveTo(430, 560).lineTo(430, 280).strokeOpacity(0.5).strokeColor("black");
-  doc.moveTo(350, 560).lineTo(350, 280).strokeOpacity(0.5).strokeColor("black");
-  doc.moveTo(300, 560).lineTo(300, 280).strokeOpacity(0.5).strokeColor("black");
-  doc.moveTo(220, 560).lineTo(220, 280).strokeOpacity(0.5).strokeColor("black");
-  doc.stroke();
-  //row border
-  console.log(`${invoiceData.customerName.replaceAll(' ','_')}_${invoiceData.invoiceNumber.replaceAll('/','_')}.pdf`)
-  res.attachment(`${invoiceData.customerName.replaceAll(' ','_')}${invoiceData.invoiceNumber.replaceAll('/','_')}.pdf`);
-  doc.pipe(res);
-  doc.end();
+    res.attachment(
+      `${invoiceData.customerName.replaceAll(
+        " ",
+        "_"
+      )}${invoiceData.invoiceNumber.replaceAll("/", "_")}.pdf`
+    );
+    doc.pipe(res);
+    doc.end();
+  } catch (error) {
+    throw new Error('SERVER BROKEN CONTACT ADMIN') 
+  }
 });
 
 const getDate = (date) => {
