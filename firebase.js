d70c7d06bf7aa83398b647dff5ac0f36dc97cd8a
pratikraidev <raipratikcse@gmail.com>
2025-04-17 00:09:22 +0530

    .env added

--- firebase.js
+++ firebase.js
@@ -2,8 +2,6 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
-
 // Initialize Firebase
 initializeApp({
   credential: cert(serviceAccount),
@@ -11,4 +9,18 @@ initializeApp({
 
 const db = getFirestore();
 
-module.exports = {db};
+module.exports = { db };
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
